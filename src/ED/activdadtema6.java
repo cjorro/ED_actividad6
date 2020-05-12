1b403a3d943779024b9913c3c114416b0aa35968
= <=>
2020-05-12 16:56:35 +0200

    modificado el mensaje

--- src/ED/activdadtema6.java
+++ src/ED/activdadtema6.java
@@ -2,7 +2,7 @@ package ED;
 
 public class activdadtema6 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola soy Clara");
     }
     
 }
