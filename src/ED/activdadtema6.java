1f8cd30c505bdc95cb8106506246a0246e292b29
= <=>
2020-05-12 17:11:35 +0200

    modificacion mensaje 3

--- src/ED/activdadtema6.java
+++ src/ED/activdadtema6.java
@@ -2,7 +2,7 @@ package ED;
 
 public class activdadtema6 {
     public static void main(String[] args) {
-        System.out.println("Hola que tal?");
+        System.out.println("Hola hoy es martes");
     }
     
 }
